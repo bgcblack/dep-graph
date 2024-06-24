d6dbbd3f201e5ff711f9a836b6b5d5c1a6452b62
sunyh <572314446@qq.com>
2024-06-24 12:04:47 +0800

    feat:提交项目

---
+++ src/components/graph/dependency-graph.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="greetings"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Graph from '@/components/graph/dependency-graph.vue'
+</script>
+
+<template>
+  <main>
+    <Graph />
+  </main>
+</template>
